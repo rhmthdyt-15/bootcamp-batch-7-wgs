c6fe4fbc324dc0b2b1502d03df1ab915c3f2682c
rhmthdyt-15 <rahmat66779@gmail.com>
2023-11-13 15:01:51 +0700

    input

--- task1/index.js
+++ task1/index.js
@@ -1,16 +1,71 @@
+const validator = require('validator');
+const fs = require('fs');
+
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  // metode nested
-  readline.question('Siapa nama kamu?', name => {
-    console.log(`Nama kamu adalah ${name}`);
-    readline.question('Berapa nomor telepon?', phone => {
-        console.log(`Nomor telepon kamu ${phone}`);
-        readline.question('Masukan email kamu?', email => {
-            console.log(`Email kamu? ${email}`)
-            readline.close();
+  input: process.stdin,
+  output: process.stdout
+});
+
+const questions = (ask) => {
+    return new Promise((resolve, reject) => {
+        readline.question(ask, (inputVariable) => {
+            resolve(inputVariable);
         })
     })
-  });
+}
+
+async function main() {
+  const dataPath = 'data.json';
+
+  let data = [];
+
+  // Pengecekan apakah file data.json ada dan tidak kosong
+  try {
+    const existingData = fs.readFileSync(dataPath, 'utf-8');
+    
+    // Memeriksa apakah existingData tidak kosong
+    if(existingData.trim() !== '') {
+      data = JSON.parse(existingData);
+
+      // Pengecekan apakah data adalah array
+      if (!Array.isArray(data)) {
+        throw new Error('Data bukan berupa array');
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  const name = await questions('Siapa nama kamu? ');
+  let phone, email;
+
+  do {
+    phone = await questions('Berapa nomor telepon? ');
+    if (validator.isMobilePhone(phone, 'id-ID')) {
+      console.log('Format nomor hp benar!');
+    } else {
+      console.log('Format nomor hp tidak valid, silakan masukkan kembali');
+    }
+  } while (!validator.isMobilePhone(phone, 'id-ID'));
+
+  do {
+    email = await questions('Masukkan email kamu? ');
+    if (validator.isEmail(email)) {
+      console.log('Format email benar!');
+    } else {
+      console.log('Format email tidak valid, silakan masukkan email kamu');
+    }
+  } while (!validator.isEmail(email));
+
+  console.log(`Nama kamu adalah ${name}`);
+  console.log(`Nomor telepon kamu ${phone}`);
+  console.log(`Email kamu? ${email}`);
+
+  data.push({ name, phone, email });
+
+  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+
+  readline.close();
+}
+
+main();
