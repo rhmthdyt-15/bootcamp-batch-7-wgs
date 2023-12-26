2bb771a55c7abdfadd0a02e285c6c61df4c28e42
rhmthdyt-15 <rahmat66779@gmail.com>
2023-12-26 19:59:52 +0700

    update format date ID

--- project/frontend/src/components/features/utils.js
+++ project/frontend/src/components/features/utils.js
@@ -2,3 +2,9 @@
 export function formatRupiah(angka) {
     return new Intl.NumberFormat('id-ID').format(angka)
 }
+
+export const formatTanggal = (tanggalISO) => {
+    const options = { day: 'numeric', month: 'long', year: 'numeric' }
+    const tanggal = new Date(tanggalISO).toLocaleDateString('id-ID', options)
+    return tanggal
+}
